f333829559e36ed486f6760a272b5cd4e01c4cf7
GoodGD <dionatha.work@gmail.com>
2025-09-25 16:50:09 -0300

    chore: fix

--- src/core/main.tsx
+++ src/core/main.tsx
@@ -1,3 +1,9 @@
+const favicon = document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.href = '/favicon.png'
+document.head.appendChild(favicon)
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
