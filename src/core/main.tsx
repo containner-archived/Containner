9d5f0083c254974847beda4b096608d9b6ebdafd
GoodGD <dionatha.work@gmail.com>
2025-09-25 17:14:42 -0300

    fix: favicon

--- src/core/main.tsx
+++ src/core/main.tsx
@@ -1,26 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@styles/global.css'
 import App from './App'
 import { I18nProvider } from '@/shared/contexts/I18nContext'
-import '@styles/global.css'
-
-const faviconUrl = '/favicon.png'
-
-const updateFavicon = () => {
-  const existingFavicon = document.querySelector('link[rel="icon"]')
-  if (existingFavicon) {
-    existingFavicon.remove()
-  }
-
-  const link = document.createElement('link')
-  link.rel = 'icon'
-  link.type = 'image/png'
-  link.href = faviconUrl
-  document.head.appendChild(link)
-}
-
-updateFavicon()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
