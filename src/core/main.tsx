781dfe3770796bf3713ee231008cb050b76d559e
GoodGD <dionatha.work@gmail.com>
2025-09-25 23:11:23 -0300

    docs: license

--- src/core/main.tsx
+++ src/core/main.tsx
@@ -5,6 +5,11 @@ import '@styles/global.css'
 import App from './App'
 import { I18nProvider } from '@/shared/contexts/I18nContext'
 
+console.log(
+  '%cDeveloped by Dionatha Goulart © 2025 "https://github.com/DionathaGoulart"',
+  'color: #ff0000; font-size: 14px; font-weight: bold;'
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
